c1ae6839e5315df188ce0e952dc2f3c1dccfb71d
Meitar Byle <maytarb@gmail.com>
2016-12-21 16:44:22 +0200

    Map's lowest point is 0

--- FighterPlane/Assets/ScaleFixer.cs
+++ FighterPlane/Assets/ScaleFixer.cs
@@ -1,17 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ScaleFixer : MonoBehaviour {
 
     private OnlineMapsBuildings onlineMapsBuildings;
+    private OnlineMapsTileSetControl onlineMapsTileSetControl;
 
     void Start()
     {
         onlineMapsBuildings = gameObject.GetComponent<OnlineMapsBuildings>();
+        onlineMapsTileSetControl = gameObject.GetComponent<OnlineMapsTileSetControl>();
         onlineMapsBuildings.OnBuildingCreated += SetBuildingScale;
         onlineMapsBuildings.OnBuildingCreated += SetGameObjectScale;
+        onlineMapsTileSetControl.OnMeshUpdated += SetMapHeight;
     }
+    private void SetMapHeight()
+    {
+        var minY = gameObject.GetComponent<MeshFilter>().sharedMesh.vertices.Select(v => v.y).Min();
+        
+        //if mesh is flat
+        if (minY == 0)
+            return;
+        
+        var transformedDistance = transform.TransformPoint(new Vector3(0, minY, 0));
+        Vector3 worldScale = GetWorldScale(transform);
+        transform.Translate(-Vector3.up * minY * GetWorldScale(transform).y, Space.World);
+        onlineMapsTileSetControl.OnMeshUpdated -= SetMapHeight;
+    }
+
+    public static Vector3 GetWorldScale(Transform transform)
+    {
+        Vector3 worldScale = transform.localScale;
+        Transform parent = transform.parent;
+
+        while (parent != null)
+        {
+            worldScale = Vector3.Scale(worldScale, parent.localScale);
+            parent = parent.parent;
+        }
+
+        return worldScale;
+    }
+
 
     private void SetGameObjectScale(OnlineMapsBuildingBase b)
     {
