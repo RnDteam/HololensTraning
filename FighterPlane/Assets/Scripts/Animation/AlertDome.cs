d02d8afcce9dfc977788d0e65df1ef6225ca17ef
Dani <BLA>
2016-12-22 16:08:16 +0200

    alert when plane is inside dome + hide and show commands

---
+++ FighterPlane/Assets/Scripts/Animation/AlertDome.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertDome : MonoBehaviour
+{
+
+    public Material ActiveAlertDome;
+    public Material NonActiveAlertDome;
+    public Material HideAlertDome;
+    private bool hideDome = true;
+    private bool isAlertActive = false;
+
+
+    private void Awake()
+    {
+        this.GetComponent<Renderer>().material = HideAlertDome;
+    }
+
+    private void OnTriggerEnter(Collider myTrigger)
+    {
+        if (myTrigger.gameObject.name.StartsWith("Hercules"))
+        {
+            this.GetComponent<Renderer>().material = ActiveAlertDome;
+            this.GetComponent<AudioSource>().Play();
+            isAlertActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider myTrigger)
+    {
+        if (myTrigger.gameObject.name.StartsWith("Hercules"))
+        {
+            this.GetComponent<Renderer>().material = (hideDome ? HideAlertDome : NonActiveAlertDome);
+            this.GetComponent<AudioSource>().Pause();
+            isAlertActive = false;
+        }
+    }
+
+    public void ShowAlert()
+    {
+        this.GetComponent<Renderer>().material = (isAlertActive ? ActiveAlertDome : NonActiveAlertDome);
+        hideDome = false;
+    }
+
+    public void HideAlert()
+    {
+        this.GetComponent<Renderer>().material = HideAlertDome;
+        hideDome = true;
+
+    }
+}
