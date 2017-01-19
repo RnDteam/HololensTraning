800fbc6b75fd033a1e6e11fa50b8650efc1544e2
zivnachum <ziv.nachum@gmail.com>
2017-01-19 15:07:22 +0200

    set planes and alert

--- FighterPlane/Assets/Scripts/Physics/GlobalManager.cs
+++ FighterPlane/Assets/Scripts/Physics/GlobalManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public static class GlobalManager {
     public static int GasThreshold = 25;
     public const double gravityMag = 9.8;
-    public const float defaultLoopRadius = 0.5f;
-    public const float defaultCircleRadius = 0.5f;
+    public const float defaultLoopRadius = 0.3f;
+    public const float defaultCircleRadius = 0.3f;
     public const float defaultLoopOmega = 0.33333f;
     public const float defaultCircleOmega = 0.33333f;
     public const float defaultAttackSpeed = 0.16667f;
