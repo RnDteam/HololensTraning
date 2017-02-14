074b5a1ffc284542e3a2ae0f228abd7f9ec1c607
ziv <ziv.nachum@gmail.com>
2017-02-14 21:20:45 +0200

    collision detection and voice control

--- FighterPlane/Assets/SadManager.cs
+++ FighterPlane/Assets/SadManager.cs
@@ -7,14 +7,42 @@ using UnityEngine;
 public class SadManager : MonoBehaviour {
 
     public bool StartFlying = false;
-    
+    public bool ApproveClimb = false;
+    public bool DisapproveClimb = false;
+
+    private void InvokeMethod(string keyword)
+    {
+        var count = KeywordManager.Instance.myKeywordsAndResponses.Count(k => k.Keywords.Contains(keyword));
+        if (count == 0)
+        {
+            Debug.LogWarning("No keyword: " + keyword);
+        }
+        else if (count > 1)
+        {
+            Debug.LogWarning("Multiple keywords: " + keyword);
+        }
+        else
+        {
+            KeywordManager.Instance.myKeywordsAndResponses.Single(k => k.Keywords.Contains(keyword)).Response.Invoke();
+        }
+    }
 
     private void Update()
     {
         if (StartFlying)
         {
             StartFlying = false;
-            KeywordManager.Instance.myKeywordsAndResponses.Single(k => k.Keywords.Contains("Start")).Response.Invoke(); ;
+            InvokeMethod("Start");
+        }
+        if (ApproveClimb)
+        {
+            ApproveClimb = false;
+            InvokeMethod("Approve");
+        }
+        if (DisapproveClimb)
+        {
+            DisapproveClimb = false;
+            InvokeMethod("Disapprove");
         }
     }
 }
